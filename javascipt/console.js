3f0f0ae163f387ca474170dd7f2e2632adcabed4
sakshi <sakshibora536@gmail.com>
2026-04-13 18:22:59 +0530

    js

---
+++ javascipt/console.js
@@ -0,0 +1,31 @@
+console.log("hello");
+let a =34;
+let b =35;
+console.log(`sum is ${a+b}`);
+let n ='addy';
+let m ='simr';
+console.log(`full name is ${n + m}`);
+
+let age=45;
+if(age>=18){
+    console.log('your age is eligible for it' );
+}
+else if (age<=18){
+console.log('you are not eligible for this program and i am really sorry for you');
+}
+let gender=34;
+if(gender>=10);
+{
+    console.log("yes i am girl ");
+if(gender>=30){
+    console.log('then ofcourse you are preety!');
+}
+}
+
+
+let names='shrey';
+if(names==='shrey');
+{console.log("you are my girl then ")  ;
+    if(names==='shrey')
+    {console.log("you are enough")};
+}
