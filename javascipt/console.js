0a4e2ea31fe90f9715f23dfcf6751d635f520848
sakshi <sakshibora536@gmail.com>
2026-04-26 17:28:22 +0530

    loop

--- javascipt/console.js
+++ javascipt/console.js
@@ -30,3 +30,33 @@ if(names==='shrey');
     {console.log("you are enough")};
 }
 
+let string='a';
+if(string==' '){
+    console.log('yes string is empty');}
+
+else (string!=' ') 
+    console.log('not available');
+
+let s='aPnacollEge';
+let index=3;
+if(s[index]==s[index].toLowerCase);{
+console.log("yes ther is lowercase");}
+ 
+let stringme= prompt('please enter your string');
+console.log(`original = ${stringme}`);
+console.log(`new = ${stringme.trim()}`);
+
+let aes=[1,2,'er'];
+let i=2;
+if(aes.indexOf(i) != -1){
+    console.log('array contain that element ');
+}
+else {
+    console.log('that element do not exist in array');
+}
+for(let i=1;i<=20;i=2+i){
+    console.log(i);
+}
+for(let i=2;i<=10;i=i+2){
+    console.log(i);
+}
