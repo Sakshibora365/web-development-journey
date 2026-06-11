b05edf8ca02be8368724ce36fa0e50805ba7249b
sakshi <sakshibora536@gmail.com>
2026-06-11 06:16:29 +0530

    added js button for add a task

--- javascipt/todo.js
+++ javascipt/todo.js
@@ -1,5 +1,11 @@
-let list = document.querySelectorAll('div');
-console.dir(list);
-if (list[1]) {
-  list[1].innerHTML = "study";
-}
+let addtask = document.querySelector('button');
+let input = document.querySelector('input');
+let list = document.querySelector('ul');
+//accessing the button and input for functonality
+
+addtask.addEventListener('click',()=>{
+  let val = input.value;
+  console.log(val);
+  list.innerHTML = list.innerHTML+`<li>${val} <button class="delete">delete</button></li>`;
+  input.value = '';
+})
