b5a3e9289873e89ca0c5cc2aab4a3194f1bd37d2
sakshi <sakshibora536@gmail.com>
2026-06-23 22:26:20 +0530

    prctise object literal

---
+++ javascipt/todo.js
@@ -0,0 +1,23 @@
+l
+
+let array =
+[ 
+ {
+    name:"sakshi",
+    class:"10",
+    age:"18"
+ }
+
+]
+//object of object 
+let object={
+    obj1 = {
+        name:satisfies,
+        class:10,
+    },
+    obj2={
+        name:a,
+        class:10,
+        },
+
+    };
