aa7fae86e79724d93120e56d2d1dce938e3fed1a
sakshi <sakshibora536@gmail.com>
2026-06-24 16:52:03 +0530

    prctise function and arrow function

--- javascipt/todo.js
+++ javascipt/todo.js
@@ -21,4 +21,17 @@ let object={
         },
 
     };
-    //creating a sum function expression 
+    setTimeout(()=>{
+        console.log("heyy cutie");
+    },2000);
+    let babygirl= setInterval(()=>{
+    console.log("hello baby girl");
+
+},2000);
+
+
+function stop(){
+    clearInterval(babygirl);
+}
+stop();
+{console.log("stop it "),3000};
