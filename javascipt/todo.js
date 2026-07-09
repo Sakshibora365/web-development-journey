c842f7538d072bca664c583af74079335e30dac8
sakshi <sakshibora536@gmail.com>
2026-07-09 15:27:55 +0530

    add button

--- javascipt/todo.js
+++ javascipt/todo.js
@@ -2,3 +2,25 @@ let button = document.getElementById('color');
 button.addEventListener("click",function(){
     button.style.backgroundColor="green";
 });
+
+let text=document.querySelector('#tsk');
+text.addEventListener("input",function(){
+console.log(text.value);
+
+
+;});
+let btn=document.querySelector('#addtask');
+let ul=document.querySelector('ul');
+let input=document.querySelector('#task');
+
+btn.addEventListener("click",function(){
+let listitem =document.createElement('li');
+listitem.innerText=input.value;
+ul.appendChild(listitem);
+input.value="";
+
+
+});
+
+
+
