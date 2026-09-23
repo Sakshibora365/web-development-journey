dff7e9405efeec99cfc6d9d7238c7ab6ffe4f943
sakshi <sakshibora536@gmail.com>
2026-09-23 19:38:04 +0530

    js for simon

--- javascipt/simon.js
+++ javascipt/simon.js
@@ -1,3 +1,38 @@
-let document.querySelector(".button").addEventListener("click", function(){
-console.log("button clicked");
+let userseq=[];
+let gameseq=[];
+
+let btns=["green","red","yellow","blue"];
+
+let started=false;
+let level=0;
+
+let pressbtn=document.querySelector(".button");
+let changebybtn=document.querySelector(".simonhead h1+p");
+let bg=document.querySelector(".simonbg");
+pressbtn.addEventListener("click",()=>{
+    if(started==false){
+        console.log("game is started");
+        started=true;
+        levelup();
+    }
+
 });
+
+function btnflash(btn){
+btn.classList.add("flash");
+setTimeout(function(){
+    btn.classList.remove("flash");
+},1000)}
+
+
+function levelup(){
+level++;
+changebybtn.innerText= `level ${level}`;
+
+//random btn to choose abut flash
+let randIndx=Math.floor(Math.random()*3);
+let randmcolor=btns[randIndx];
+let randmbtn=document.querySelector(`.${randmcolor}`);
+console.log(randmbtn);
+btnflash(randmbtn);
+}
